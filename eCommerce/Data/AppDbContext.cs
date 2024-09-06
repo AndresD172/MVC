968615d7d6237b2dee3693dae95b01b7846ac773
Estudiante-03 <Estudiante-03@DESKTOP-179SH2Q>
2024-09-06 14:55:10 -0600

    Agregar archivos de proyecto.

---
+++ eCommerce/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // Agregamos los modelos correspondientes a cada tabla de la base de datos
+        DbSet<Category> category { get; set; }
+    }
+}
